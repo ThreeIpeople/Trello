af035c19380bcffbf534e04b1bbe46904f0a3fe0
kudongku <kudongku@naver.com>
2024-03-20 15:35:14 +0900

    ReFactor : ResponseEntity로 리턴값 통일

--- src/main/java/com/sparta/trellowiththreeipeople/bar/controller/BarController.java
+++ src/main/java/com/sparta/trellowiththreeipeople/bar/controller/BarController.java
@@ -16,29 +16,33 @@ public class BarController {
     private final BarService barService;
 
     @PostMapping
-    public void createBar(@PathVariable Long boardId, @RequestBody BarRequestDto barRequestDto) {
+    public ResponseEntity<String> createBar(@PathVariable Long boardId, @RequestBody BarRequestDto barRequestDto) {
         barService.createBar(boardId, barRequestDto);
+        return ResponseEntity.ok().body("Bar가 정상적으로 생성되었습니다.");
     }
 
     @GetMapping
     public ResponseEntity<List<BarResponseDto>> getBarList(@PathVariable Long boardId) {
         List<BarResponseDto> barResponseDtoList = barService.getBarList(boardId);
-        return ResponseEntity.status(200).body(barResponseDtoList);
+        return ResponseEntity.ok().body(barResponseDtoList);
     }
 
     @GetMapping("/{barId}")
     public ResponseEntity<BarResponseDto> getBar(@PathVariable Long boardId, @PathVariable Long barId) {
         BarResponseDto barResponseDto = barService.getBar(boardId, barId);
-        return ResponseEntity.status(200).body(barResponseDto);
+        return ResponseEntity.ok().body(barResponseDto);
     }
 
     @PutMapping("/{barId}")
-    public void updateBar(@PathVariable Long boardId, @PathVariable Long barId, @RequestBody BarRequestDto barRequestDto) {
+    public ResponseEntity<String> updateBar(@PathVariable Long boardId, @PathVariable Long barId, @RequestBody BarRequestDto barRequestDto) {
         barService.updateBar(boardId, barId, barRequestDto);
+        return ResponseEntity.ok().body("Bar가 정상적으로 수정되었습니다.");
+
     }
 
     @DeleteMapping("/{barId}")
-    public void deleteBar(@PathVariable Long boardId, @PathVariable Long barId) {
+    public ResponseEntity<String> deleteBar(@PathVariable Long boardId, @PathVariable Long barId) {
         barService.deleteBar(boardId, barId);
+        return ResponseEntity.ok().body("Bar가 정상적으로 삭제되었습니다.");
     }
 }
