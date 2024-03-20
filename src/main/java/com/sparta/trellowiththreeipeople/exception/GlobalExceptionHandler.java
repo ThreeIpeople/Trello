31518ef0a80e3ad8978b996a0d4790bd8eaed784
kiwimel0n <129644629+kiwimel0n@users.noreply.github.com>
2024-03-20 16:29:54 +0900

    Refactor : 코드리팩토링 (#13)
    * refactor : 코드수정
    
    * refactor : 코드 수정
    
    * refactor : entity BaseEntity 상속받도록 수정

--- src/main/java/com/sparta/trellowiththreeipeople/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/trellowiththreeipeople/exception/GlobalExceptionHandler.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
+
     public ResponseEntity<String> IllegalArgumentException(IllegalArgumentException e) {
+
         return ResponseEntity.badRequest()
                 .body(e.getMessage());
     }
@@ -21,6 +23,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(UserNotFoundException.class)
+
     public ResponseEntity<String> UserNotFoundException(UserNotFoundException e) {
         return ResponseEntity.badRequest()
                 .body(e.getMessage());
@@ -28,6 +31,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BoardNotFoundException.class)
     public ResponseEntity<String> BoardNotFoundException(BoardNotFoundException e) {
+
         return ResponseEntity.badRequest()
                 .body(e.getMessage());
     }
