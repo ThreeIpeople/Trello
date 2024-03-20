d58a81fdf83ec95e089457fe4d4bb13a70bd9639
kudongku <kudongku@naver.com>
2024-03-20 19:32:14 +0900

    Refactor : test를 위해 AllArgsConstructor를 CreateUserRequestDto에 추가

--- src/main/java/com/sparta/trellowiththreeipeople/user/dto/request/CreateUserRequestDto.java
+++ src/main/java/com/sparta/trellowiththreeipeople/user/dto/request/CreateUserRequestDto.java
@@ -1,8 +1,10 @@
 package com.sparta.trellowiththreeipeople.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CreateUserRequestDto {
     private String username;
     private String password;
